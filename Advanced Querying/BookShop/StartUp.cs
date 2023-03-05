e4ce1edc351115edd96eccb3b2f0af4b76757aad
CECO <cvetomir97@abv.bg>
2023-03-05 11:31:39 +0200

    Adding new method - P02 Exercise from advanced querying

--- Advanced Querying/BookShop/StartUp.cs
+++ Advanced Querying/BookShop/StartUp.cs
@@ -1,7 +1,9 @@
 ﻿namespace BookShop
 {
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
+    using System.Text;
 
     public class StartUp
     {
@@ -9,7 +11,38 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
+
+            string? input = Console.ReadLine();
+            string result = GetBooksByAgeRestriction(db, input);
+            Console.WriteLine(result);
         }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            var books = context.Books
+                .OrderBy(b => b.Title)
+                .ToArray()
+                .Select(b => new
+                {
+                    AgerestrictionTostring = b.AgeRestriction.ToString().ToUpper(),
+                    BookTitle = b.Title,
+                })
+                .Where(b => b.AgerestrictionTostring == command.ToUpper())
+                .ToArray();
+                
+               
+                
+                
+            foreach ( var book in books ) 
+            {
+                stringBuilder.AppendLine( book.BookTitle );
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+
     }
 }
 
