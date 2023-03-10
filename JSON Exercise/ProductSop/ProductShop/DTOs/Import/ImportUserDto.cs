6f4b00e499fe069803c60a3baf43f60d70dd1a8a
CECO <cvetomir97@abv.bg>
2023-03-10 20:52:09 +0200

    ProductShop exercise

---
+++ JSON Exercise/ProductSop/ProductShop/DTOs/Import/ImportUserDto.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace ProductShop.DTOs.Import;
+
+[JsonObject]
+
+public class ImportUserDto
+{
+    [JsonProperty("firstName")]
+    public string FirstName { get; set; }
+
+    [JsonProperty("lastName")]
+    public string LastName { get; set; }
+    
+    [JsonProperty("age")]
+    public int? Age { get; set; }
+}
---
+++ JSON Exercise/ProductSop/ProductShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+
+       
+        public static void Main()
+        {
+        
+            
+            
+            ProductShopContext dbContext = new ProductShopContext();
+
+            string inputJson = File.ReadAllText("../../../Datasets/users.json");
+
+
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+            string output = ImportUsers(dbContext, inputJson);
+            Console.WriteLine(output);
+        }
+
+       
+    }
+}
