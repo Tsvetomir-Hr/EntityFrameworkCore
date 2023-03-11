a778ebddd9dfc035a83b095d00562769bcc8191c
CECO <cvetomir97@abv.bg>
2023-03-11 15:57:09 +0200

    DB First exercise

---
+++ DBFirstModelExercise/DBFirstModelExercise/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+           SoftUniContext context = new SoftUniContext();
+            string result = GetEmployeesFullInformation(context);
+            Console.WriteLine(result);
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //Every LINQ before ToArray() present to sql query and is it used after ToArray() NOT present in sql query
+
+            //It is better to use LINQ before ToArray() for better performance!!!
+
+            var allEmployees = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToArray();
+
+            foreach (var employee in allEmployees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.LastName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
