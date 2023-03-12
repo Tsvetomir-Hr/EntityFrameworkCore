f75bfd0abd33efcb994f54ab8f9149e1f5de87de
CECO <cvetomir97@abv.bg>
2023-03-12 10:59:05 +0200

    CarDealer db  - XML import and export exrcises.

---
+++ XML Exercise/CarDealer/DTOs/Import/ImportSupplierDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import;
+
+
+[XmlType("Supplier")]
+public class ImportSupplierDto
+{
+    //xml elements
+    [XmlElement("name")]
+    public string Name { get; set; }
+
+    [XmlElement("isImporter")]
+    public bool IsisImporter { get; set; }
+
+}
---
+++ XML Exercise/CarDealer/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext dbContext = new CarDealerContext();
+
+            string xml = File.ReadAllText("../../../Datasets/suppliers.xml");
+
+            string result = ImportSuppliers(dbContext, xml);
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Suppliers"); // root
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportSupplierDto[]), xmlRoot);
+
+            using StringReader xmlReader = new StringReader(inputXml);
+
+            ImportSupplierDto[] dtos = (ImportSupplierDto[])xmlSerializer
+                .Deserialize(xmlReader);
+
+            Supplier[] suppliers = dtos.Select(s => new Supplier
+            {
+                Name = s.Name,
+                IsImporter = s.IsisImporter
+            })
+                .ToArray();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
+        }
+
+        private static T Desirialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName); // root
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringReader xmlReader = new StringReader(inputXml);
+            T dtos = (T)xmlSerializer
+                .Deserialize(xmlReader);
+
+            return dtos;
+        }
+    }
+}
