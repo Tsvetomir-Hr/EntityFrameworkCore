0b630d7dfaadc2d72f6f903ab5c4bc2d4492bc7d
CECO <cvetomir97@abv.bg>
2023-03-13 23:42:58 +0200

    Adding new method for importing users from json file

---
+++ JSON Exercise/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace ProductShop;
+
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+public class StartUp
+{
+
+    public static void Main()
+    {
+
+        ProductShopContext context = new ProductShopContext();
+        string inputJson = File.ReadAllText("../../../Datasets/users.json");
+
+        string result = ImportUsers(context, inputJson);
+
+        Console.WriteLine(result);
+    }
+    public static string ImportUsers(ProductShopContext context, string inputJson)
+    {
+        IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<ProductShopProfile>();
+        }));
+        InportUserDto[] userDtos = JsonConvert.DeserializeObject<InportUserDto[]>(inputJson);
+
+        ICollection<User> validUser = new HashSet<User>();
+
+        foreach (var userDto in userDtos)
+        {
+            User user = mapper.Map<User>(userDto);
+
+            validUser.Add(user);
+        }
+
+        context.Users.AddRange(validUser);
+        context.SaveChanges();
+
+        return $"Successfully imported {validUser.Count}";
+    }
+}
+
+
