2c9d47607ed1d7b3fa905d33dfc29760523634f2
CECO <cvetomir97@abv.bg>
2023-03-14 23:34:45 +0200

    fix

---
+++ JSON Exercise/ProductShop/ProductShop/DTOs/Import/ImportCategorieDto.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace ProductShop.DTOs.Import;
+
+[JsonObject]
+public class ImportCategorieDto
+{
+    [JsonProperty("name")]
+    public string? Name { get; set; }
+}
---
+++ JSON Exercise/ProductShop/ProductShop/DTOs/Import/ImportCategoryProductsDto.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace ProductShop.DTOs.Import;
+
+[JsonObject]
+public class ImportCategoryProductsDto
+{
+    [JsonProperty("CategoryId")]
+    public int CategoryId { get; set; }
+
+    [JsonProperty("ProductId")]
+    public int ProductId { get; set; }
+}
