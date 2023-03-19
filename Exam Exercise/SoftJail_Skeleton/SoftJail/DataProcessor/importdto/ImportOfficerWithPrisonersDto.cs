72bc1cc78fa0e0f3b9891908e1b0860d7d0ba3a9
CECO <cvetomir97@abv.bg>
2023-03-19 09:36:04 +0200

    Added method for importing data from xml file

---
+++ Exam Exercise/SoftJail_Skeleton/SoftJail/DataProcessor/importdto/ImportOfficerWithPrisonersDto.cs
@@ -0,0 +1,38 @@
+﻿using SoftJail.Common;
+using SoftJail.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.importdto
+{
+    [XmlType("Officer")]
+    public class ImportOfficerWithPrisonersDto
+    {
+        [XmlElement("Name")]
+        [MinLength(ValidationConstants.OfficerFullNameMinLength)]
+        [MaxLength(ValidationConstants.OfficerFullNameMaxLength)]
+        [Required]
+        public string FullName { get; set; }
+
+        [XmlElement("Money")]
+        [Range(typeof(decimal), ValidationConstants.OfficerMinSalary, ValidationConstants.OfficerMaxSalary)]
+        public decimal Salary { get; set; }
+
+        [XmlElement("Position")]
+        [Required]
+        public string Position { get; set; }
+
+        [XmlElement("Weapon")]
+        [Required]
+        public string Weapon { get; set; }
+
+        [XmlElement("DepartmentId")]
+        public int DeaprtmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public ImportOfficerPrisonerDto[] OfficerPrisoners { get; set; }
+    }
+}
