b10439924043ad0a399e7490ee1e364a07ea77fb
CECO <cvetomir97@abv.bg>
2023-03-25 10:18:46 +0200

    added the exportDtos classes

---
+++ Exam Exercise/Artillery/Artillery/DataProcessor/exportdto/ExpoertContriesDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ExportDto
+{
+    [XmlType("Country")]
+    public class ExpoertContriesDto
+    {
+        [XmlAttribute("Country")]
+        public string CountryName { get; set; } = null!;
+
+        [XmlAttribute("ArmySize")]
+        public int ArmySize { get; set; }
+    }
+}
