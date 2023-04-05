99961b004a2838979e9ca0de6ceb4e533d65bd34
CECO <cvetomir97@abv.bg>
2023-04-05 22:57:19 +0300

    clear warnings

--- Exam Exercise/Trucks/Trucks/Data/Models/ClientTruck.cs
+++ Exam Exercise/Trucks/Trucks/Data/Models/ClientTruck.cs
@@ -11,11 +11,11 @@ namespace Trucks.Data.Models
     {
         [ForeignKey(nameof(Client))]
         public int ClientId { get; set; }
-        public virtual Client Client { get; set; } 
+        public virtual Client Client { get; set; } = null!;
 
 
         [ForeignKey(nameof(Truck))]
         public int TruckId { get; set; }
-        public virtual Truck Truck { get; set; } 
+        public virtual Truck Truck { get; set; } = null!;
     }
 }
