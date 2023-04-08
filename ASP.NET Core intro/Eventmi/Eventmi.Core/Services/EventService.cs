6ab5623676f951e7d4a5349929845b9891a4670d
CECO <cvetomir97@abv.bg>
2023-04-08 18:13:05 +0300

    Added generic repository similar to the repository pattern(Stamo approach).

--- ASP.NET Core intro/Eventmi/Eventmi.Core/Services/EventService.cs
+++ ASP.NET Core intro/Eventmi/Eventmi.Core/Services/EventService.cs
@@ -1,21 +1,30 @@
 ﻿using Eventmi.Core.Contracts;
 using Eventmi.Core.Models;
+using Eventmi.Infrastructure.Data.Common;
 using Eventmi.Infrastructure.Data.Models;
 
 namespace Eventmi.Core.Services
 {
     public class EventService : IEventService
     {
+        private readonly IRepository repo;
+        public EventService(IRepository _repo)
+        {
+            this.repo = _repo;
+        }
 
         public async Task AddAsync(EventDetailsModel model)
         {
-           Event entity = new Event()
-        {
-               Name = model.Name,
-               End = model.End,
-               Start = model.Start,
-               Place = model.Place
-        };
+            Event entity = new Event()
+            {
+                Name = model.Name,
+                End = model.End,
+                Start = model.Start,
+                Place = model.Place
+            };
+            await repo.AddAsync(entity);
+            await repo.SaveChangesAsync();
+
         }
 
         public async Task DeleteAsync(int id)
