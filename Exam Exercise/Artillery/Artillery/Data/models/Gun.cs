4a86648e28b5828152a51d08955298586a2ddb20
CECO <cvetomir97@abv.bg>
2023-04-25 23:56:01 +0300

    Removed unnecessarry usings

--- Exam Exercise/Artillery/Artillery/Data/models/Gun.cs
+++ Exam Exercise/Artillery/Artillery/Data/models/Gun.cs
@@ -1,11 +1,6 @@
 ﻿using Artillery.Data.Models.Enums;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Artillery.Data.Models
 {
--- Exam Exercise/Artillery/Artillery/Data/models/enums/GunType.cs
+++ Exam Exercise/Artillery/Artillery/Data/models/enums/GunType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Artillery.Data.Models.Enums
 {
     public enum GunType
